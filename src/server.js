fb53116af66a245719da5287578328f33ee966e6
joao vitor <resetgamer1@gmail.com>
2020-01-28 13:25:29 -0300

    git ignore added

--- src/server.js
+++ src/server.js
@@ -1,48 +1,17 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send("Olá você");
-})
+});
 
-app.post('/home', verifyToken, (req, res) => {
-	res.send('Home')
-})
+app.use(express.json());
+app.use(routes);
+// DB connection
 
-app.post('/login', (req, res) => {
-  const user = {
-    id: 1,
-    username: 'brad',
-    email: 'brad@gmail.com'
-  };
-
-	jwt.sign({user}, 'secretkey', (err, token) => {
-    res.json({
-			token
-    });
-	});
-	
-})
-
-// Token format
-// Authorization: Bearer <access_token>
-
-
-
-function verifyToken(req, res, next) {
-	// Get auth header value
-	const bearerHeader = req.headers['authorization'];
-	// Check if bearer is undefined 
-	if(typeof bearerHeader !== 'undefined') {
-		
-	} else {	
-		// Forbidden
-		res.sendStatus(403);
-	}
-
-}
-;;;
 app.listen(3333);
 
