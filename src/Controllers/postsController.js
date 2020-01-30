8ed83dfd171e559c957478fbc226017ea6a17cf5
joao vitor <resetgamer1@gmail.com>
2020-01-29 21:12:18 -0300

    posts collection find

--- src/Controllers/postsController.js
+++ src/Controllers/postsController.js
@@ -1,18 +1,22 @@
 const User = require('../models/User');
-const verify = require('../token/verifyToken');
+const Post = require('../models/Post');
 
 module.exports = {
   async index(req, res) {
+    const { tweet } = req.body;
 
-    const { tweet, likes } = req.body;
-    // Get Author from Cached Token
-    const author = (req.user._id);
-
-    // Save post into array in user collection
-    const user = await User.findOne({ _id: author });
-    user.posts.push({ tweet, likes, author });
-    user.save();
-
-    return res.json(user);
-  } 
+    if(!tweet) {
+      const posts = await Post.find();
+      return res.json(posts);
+    }else {
+      // Get Author from Cached Token
+      const author = (req.user._id);
+      // On click count one like
+      const likes = 0;
+      // Save post into array in user collection
+      const post = await Post.create({ tweet, likes, author });
+      const posts = await Post.find();
+      return res.json(posts);
+    }
+  }
 }
--- src/models/Post.js
+++ src/models/Post.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-  tweet: String,
-  likes: Number,
-  author: String
+      tweet: String,
+      likes: Number,
+      author: String
 });
 
 module.exports = mongoose.model('Post', PostSchema);
