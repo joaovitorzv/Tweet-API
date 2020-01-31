98758f766142b7bf3b40d7e67122410b13566d3d
joao vitor <resetgamer1@gmail.com>
2020-01-31 13:10:11 -0300

    finished

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const env = require('dotenv').config();
 
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 
 // DB connection
 mongoose.connect(process.env.DB_CONNECT,{ 
--- src/token/verifyToken.js
+++ src/token/verifyToken.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
   const token = req.header('auth-token');
-  if(!token) return res.status(401).json({ message: 'Access denied' });
+  if(!token) return res.status(401).json({ access: false });
 
   try{
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
     next();
   } catch(err) {
-    res.status(400).send('Invalid token');
+    res.status(400).json({ access: false });
   }
 }
